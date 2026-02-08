777b9b3619b082985c96ab698df6ace0de05570f
CamiloMedel <camilomedel3548@gmail.com>
2026-02-08 13:34:26 -0600

    Initial Commit

---
+++ ProductifyTests/ProductifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProductifyTests.swift
+//  ProductifyTests
+//
+//  Created by Camilo Medel on 2/8/26.
+//
+
+import Testing
+@testable import Productify
+
+struct ProductifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
